504b05ff0de540990a8ae0df732f9d1a74d7d3f9
Sep Dehpour <sdehpour@zehnergroup.com>
2013-04-03 20:15:31 -0700

    changed tabs to spaces

--- git_delete_remote_branches.py
+++ git_delete_remote_branches.py
@@ -6,11 +6,11 @@ out, err = p.communicate()
 out2 = out.splitlines()
 
 for i in out2:
-	if not i.endswith("master"):
-		b = i[17:]
-		print b
-		p = subprocess.Popen(['git', 'push', 'origin', '--delete', b], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-		out, err = p.communicate()
-		print out
-		print "err", err
+    if not i.endswith("master"):
+        b = i[17:]
+        print b
+        p = subprocess.Popen(['git', 'push', 'origin', '--delete', b], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        print out
+        print "err", err
 
