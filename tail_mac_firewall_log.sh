b51f8ea839c786ad68a15e28266495f0575661d9
Sep Dehpour <sep@zepworks.com>
2015-04-06 19:00:54 -0700

    Mac firewall tail to read PF firewall in addition to app firewall

--- tail_mac_firewall_log.sh
+++ tail_mac_firewall_log.sh
@@ -1,4 +1,4 @@
-sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setloggingmode on 
-sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setloggingopt detail 
-sudo tail -f /var/log/appfirewall.log 
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setloggingmode on
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setloggingopt detail
+multitail -s 2 -i /var/log/appfirewall.log -i /var/log/pffirewall.log
 sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setloggingopt throttled
